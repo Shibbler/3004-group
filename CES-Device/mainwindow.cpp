e23baa3e232e7c05c4dd1ea965f350507afcdeee
student <lucascolwell@gmail.com>
2022-04-08 16:09:32 -0400

    Added powerButton functionality

--- CES-Device/mainwindow.cpp
+++ CES-Device/mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "Session.h"
-
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -9,6 +9,22 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+
+    connect(ui->PowerButton, SIGNAL (pressed()) , this, SLOT (powerClickedHeld()));
+
+    connect(ui->PowerButton, SIGNAL (released()) , this, SLOT (softOffFromButton()));
+
+    this->batteryTimer = new QTimer(this);
+    this->softOffTimer = new QTimer(this);
+    connect(batteryTimer, SIGNAL (timeout()),this, SLOT (drainBattery()));
+    connect(softOffTimer, SIGNAL (timeout()),this, SLOT (softOffTimed()));
+
+
+    //set batteryPower to 100
+    this->batteryLevel = 100.0;
+    //set in session bool to false
+    this->inSession = true;
+    //this->softOffRow = 8;
 
     //TESTING
     Session *test = new Session(0,TWENTY_MIN,DELTA,1);
@@ -23,6 +39,55 @@ MainWindow::MainWindow(QWidget *parent)
     //at startup we can read the "DB" textfile and load in all the saved sessions. To start out, the DB file is filled with placeholder default sessions.
 
 
+}
+
+//function to be called for battery drainage (should be timed)
+void MainWindow::drainBattery(){
+
+}
+
+//call the softoffbased on button press
+void MainWindow::softOffFromButton(){
+    if (this->inSession == true){//only call if inSession, otherwise softOff not required
+        this->softOffRow = 0;
+        ui->SessionType_2->setCurrentRow(this->softOffRow,QItemSelectionModel::Select);
+        for (int i = 0; i< 8; i++){//start 5 timers (this will be a bit hacky, but it works.
+            ui->SessionType_2->setCurrentRow(8);
+            softOffTimer->start(500);
+        }
+    }
+    //ensure booleans are nice again
+    this->inSession = false;
+    this->powerStatus = false;
+    this->softOffRow = 0;
+    ui->powerLabel->setText("POWER: OFF");
+}
+
+void MainWindow::softOffTimed(){
+    if (this->softOffRow <=8){
+       ui->SessionType_2->setCurrentRow(this->softOffRow,QItemSelectionModel::Deselect);
+       this->softOffRow++;
+       ui->SessionType_2->setCurrentRow(this->softOffRow, QItemSelectionModel::Select);
+       qDebug() << "The row is: " << this->softOffRow;
+    }else{
+        this->softOffTimer->stop();
+    }
+}
+
+//models a very simple case of turning the  device on, or off by HOLDING the button, might need to be more complex
+void MainWindow::powerClickedHeld(){ //we can call this a hard off
+    if (this->powerStatus == false){//if trying to turn on
+        if (this->batteryLevel >0){
+            ui->powerLabel->setText("POWER: ON");
+           // batteryTimer->start(1000); //starts the battery drain THIS SHOULD PROBABLY ONLY DRAIN WITH SESSION
+            this->powerStatus = true;
+        }else{
+            ui->powerLabel->setText("NO CHARGE");
+        }
+    }else{//if trying to turn off
+        this->powerStatus = false;
+        ui->powerLabel->setText("POWER: OFF");
+    }
 }
 
 MainWindow::~MainWindow()
--- CES-Device/mainwindow.h
+++ CES-Device/mainwindow.h
@@ -2,7 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QString>
+#include <QTimer>
+#include "Session.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -14,11 +16,28 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    QTimer *batteryTimer;
+    QTimer *softOffTimer;
+
 
 private:
     Ui::MainWindow *ui;
     Session *savedSessions[10];
     Session *curSession;
+    float batteryLevel;
+    int numSessions;
+    bool inSession;
+    int softOffRow;
+    bool powerStatus;
+
+public slots:
+    void powerClickedHeld();
+    void drainBattery();
+    void softOffFromButton();
+    void softOffTimed();
+signals:
+
+
 
 
 };
